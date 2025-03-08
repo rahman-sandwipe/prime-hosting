f7441dd422808ff6aa703bd338c0b1a9f2939f96
M Rahman <ctgeye.news@gmail.com>
2025-03-08 11:42:59 +0600

    Fresh Project

---
+++ public/frontend/js/secure.js
@@ -0,0 +1,1 @@
+document.addEventListener("contextmenu",(e=>e.preventDefault())),document.onkeydown=function(e){return 123==e.keyCode?(alert("F12 Disable"),!1):e.ctrlKey&&67==e.keyCode?(alert("ctrl + c disable"),!1):e.ctrlKey&&85==e.keyCode?(alert("ctrl + u disable"),!1):void 0};
