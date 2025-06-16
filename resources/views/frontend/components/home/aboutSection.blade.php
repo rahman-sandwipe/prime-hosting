894aac1e67a0f86547bb22cda05efc10d44e86a4
Morex Devs <morexdevs@gmail.com>
2025-06-16 17:51:31 +0600

    HomePage:aboutSection Blade, Controller & Route  File

--- resources/views/frontend/components/home/aboutSection.blade.php
+++ resources/views/frontend/components/home/aboutSection.blade.php
@@ -3,27 +3,34 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="about-image">
-                    <img class="company" src="{{ asset('images/partials/ab01.jpg') }}" alt="about">
-                    <img class="people" src="{{ asset('images/partials/ab02.jpg') }}" alt="about">
+                    <span class="companyImage">
+                        <img class="company" src="" alt="about">
+                    </span>
+                    <span class="peopleImage">
+                        <img class="people" src="" alt="about">
+                    </span>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="about-content">
-                    <h2 class="section-subtitle">about domhost</h2>
-                    <h3 class="section-title">
-                        <span>Domhost believes</span>
-                        that your business is
-                        <span>unique</span>
-                        in your right ideas online.
+                    <h2 class="section-subtitle" id="aboutSubtitle">about domhost</h2>
+                    <h3 class="section-title" id="aboutTitle">
+                        <!-- This title can be dynamically set via JavaScript if needed -->
                     </h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil nemo, animi maiores saepe debitis libero necessitatibus rerum dolores ex. Nisi eveniet mollitia voluptatum. Error, accusamus repudiandae molestias excepturi beatae similique illo quo provident architecto fugiat.</p>
+                    <p id="aboutDescription">
+                        <!-- This description can be dynamically set via JavaScript if needed -->
+                    </p>
                     <ul>
                         <li>
-                            <h5>2343+</h5>
+                            <h5 id="aboutRegisteredUsers">
+                                <!-- This user can be dynamically set via JavaScript if needed -->
+                            </h5>
                             <p>registered users</p>
                         </li>
                         <li>
-                            <h5>7685+</h5>
+                            <h5 id="aboutCurrentlyHosted">
+                                <!-- This number can be dynamically set via JavaScript if needed -->
+                            </h5>
                             <p>currently hosted</p>
                         </li>
                     </ul>
@@ -32,3 +39,29 @@
         </div>
     </div>
 </section>
+
+<script>
+    // Load about section data
+    $(document).ready(function() {
+        $.ajax({
+            url: '/about-section',
+            type: 'GET',
+            success: function(response) {
+                let aboutData = response.aboutData;
+                // Set content
+                $('#aboutSubtitle').text(aboutData.subtitle || 'about domhost');
+                $('#aboutTitle').text(aboutData.title || 'About Us');
+                $('#aboutDescription').text(aboutData.description || 'Lorem ipsum dolor sit amet');
+                $('#aboutRegisteredUsers').text((aboutData.registeredUsers || '1,000') + ' +');
+                $('#aboutCurrentlyHosted').text((aboutData.currentlyHosted || '12,345') + ' +');
+                // Set images
+                $('.companyImage img').attr('src', aboutData.image1 || '/images/partials/default.jpg');
+                $('.peopleImage img').attr('src', aboutData.image2 || '/images/partials/default.jpg');
+            },
+            error: function(xhr, status, error) {
+                console.error('Error loading about section:', error);
+            }
+        });
+    });
+</script>
+<!-- End of about section -->
